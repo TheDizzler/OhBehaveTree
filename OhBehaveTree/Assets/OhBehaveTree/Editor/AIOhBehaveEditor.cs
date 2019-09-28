b75394f763d40423c9beaced4767c787f209594c
TheDizzler <tgillon.music@gmail.com>
2019-09-28 19:48:01 +0900

    statemachine created; can create statemachine in custom folder

--- OhBehaveTree/Assets/OhBehaveTree/Editor/AIOhBehaveEditor.cs
+++ OhBehaveTree/Assets/OhBehaveTree/Editor/AIOhBehaveEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 namespace AtomosZ.OhBehave.CustomEditors
@@ -7,16 +8,26 @@ namespace AtomosZ.OhBehave.CustomEditors
 	[CanEditMultipleObjects]
 	public class AIOhBehaveEditor : Editor
 	{
+		private const string DefaultNodeFolder = "OhBehaveNodes";
+		private const string UserNodeFolderKey = "UserNodeFolder";
+
+		private string userNodeFolder;
 		private AIOhBehave instance;
 		private SerializedProperty aiBehaviourTree;
 		private Editor cachedEditor;
+
 
 
 		public void OnEnable()
 		{
 			instance = (AIOhBehave)target;
 			aiBehaviourTree = serializedObject.FindProperty("ai");
-			//cachedEditor = Editor.CreateEditor(target);
+
+			userNodeFolder = EditorPrefs.GetString(UserNodeFolderKey, "");
+			if (userNodeFolder == "")
+			{
+				userNodeFolder = DefaultNodeFolder;
+			}
 		}
 
 
@@ -33,20 +44,43 @@ namespace AtomosZ.OhBehave.CustomEditors
 			{
 				if (GUILayout.Button("Create AI Tree"))
 				{
+					if (!AssetDatabase.IsValidFolder("Assets/" + userNodeFolder))
+					{
+						if (!AssetDatabase.IsValidFolder("Assets/" + DefaultNodeFolder))
+							AssetDatabase.CreateFolder("Assets", DefaultNodeFolder);
+						userNodeFolder = DefaultNodeFolder;
+						EditorPrefs.SetString(UserNodeFolderKey, userNodeFolder);
+					}
+
+					string nodename = "NewOhBehaveStateMachine";
+					int num = AssetDatabase.FindAssets(nodename, new string[] { "Assets/" + userNodeFolder }).Length;
+					if (num != 0)
+					{
+						nodename += " (" + num + ")";
+					}
+
 					var path = EditorUtility.SaveFilePanelInProject(
-						"Create New Node Root", "NewRootNode", "asset", "Where to save node?");
+						"Create New Node Root", nodename, "asset", "Where to save node?", "Assets/" + userNodeFolder);
 					if (path.Length != 0)
 					{
-						var node = CreateInstance<SelectorNode>();
-						aiBehaviourTree.objectReferenceValue = node;
-						AssetDatabase.CreateAsset(node, path);
-						EditorWindow.GetWindow<OhBehaveEditorWindow>().Open(node);
+						// check if user is using a folder that isn't the default
+						if (Path.GetFileName(Path.GetDirectoryName(path)) != userNodeFolder)
+						{
+							userNodeFolder = Path.GetFileName(Path.GetDirectoryName(path));
+							EditorPrefs.SetString(UserNodeFolderKey, userNodeFolder);
+						}
+
+						var statemachine = CreateInstance<OhBehaveStateMachineController>();
+						statemachine.Initialize(path);
+						aiBehaviourTree.objectReferenceValue = statemachine;
+						EditorWindow.GetWindow<OhBehaveEditorWindow>().Open(statemachine);
 					}
 				}
 			}
 			else if (GUILayout.Button("Open AIOhBehaveEditor"))
 			{
-				EditorWindow.GetWindow<OhBehaveEditorWindow>().Open((SelectorNode)aiBehaviourTree.objectReferenceValue);
+				EditorWindow.GetWindow<OhBehaveEditorWindow>().Open(
+					(OhBehaveStateMachineController)aiBehaviourTree.objectReferenceValue);
 			}
 			//else if (clickArea.Contains(Event.current.mousePosition) && Event.current.type == EventType.MouseDown)
 			//{
---
+++ OhBehaveTree/Assets/OhBehaveTree/OhBehaveStateMachineController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace AtomosZ.OhBehave
+{
+	public class OhBehaveStateMachineController : ScriptableObject
+	{
+		public ICompositeNode parentNode;
+
+
+		public void Initialize(string path)
+		{
+			AssetDatabase.CreateAsset(this, path);
+			// add root node to statemachine
+			parentNode = CreateInstance<SelectorNode>();
+			AssetDatabase.AddObjectToAsset(parentNode, path); 
+		}
+	}
+}
