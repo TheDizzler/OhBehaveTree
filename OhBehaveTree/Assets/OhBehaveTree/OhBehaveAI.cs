321bbc44a13c46b9dff5c7b60f3d2eee084ed814
TheDizzler <t.gillon@dico4u.com>
2020-08-06 20:13:23 +0900

    in-game deserialization (untested)

--- OhBehaveTree/Assets/OhBehaveTree/OhBehaveAI.cs
+++ OhBehaveTree/Assets/OhBehaveTree/OhBehaveAI.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 namespace AtomosZ.OhBehave
@@ -15,10 +17,65 @@ namespace AtomosZ.OhBehave
 		private INode currentNode;
 
 
-		public void SetRoot(ICompositeNode rootNode)
+		void Start()
 		{
-			root = rootNode;
-			currentNode = root.Init();
+			StreamReader reader = new StreamReader(Application.streamingAssetsPath + jsonFilepath);
+			string fileString = reader.ReadToEnd();
+			reader.Close();
+
+			JsonBehaviourTree tree = JsonUtility.FromJson<JsonBehaviourTree>(fileString);
+
+			Dictionary<int, INode> nodeDict = new Dictionary<int, INode>();
+			List<INode> nodes = new List<INode>();
+
+			foreach (JsonNodeData nodeData in tree.tree)
+			{
+				INode newNode;
+				switch (nodeData.nodeType)
+				{
+					case NodeType.Inverter:
+						newNode = new InverterNode();
+						break;
+					case NodeType.Leaf:
+						newNode = new LeafNode();
+						Type sourceType = tree.actionSource.GetType();
+						if (sourceType.GetMethod(nodeData.methodInfoName) == null)
+							throw new Exception("Could not find method by name " + nodeData.methodInfoName);
+						((LeafNode)newNode).actionInfo = sourceType.GetMethod(nodeData.methodInfoName);
+						break;
+					case NodeType.Selector:
+						newNode = new SelectorNode();
+						break;
+					case NodeType.Sequence:
+						newNode = new SequenceNode();
+						break;
+					default:
+						throw new System.Exception("Node has no type!");
+				}
+
+				nodeDict[nodeData.index] = newNode;
+			}
+
+			foreach (JsonNodeData nodeData in tree.tree)
+			{
+				if (nodeData.childrenIndices != null)
+				{
+					ICompositeNode node = (ICompositeNode)nodeDict[nodeData.index];
+					node.children = new List<INode>();
+					foreach (int child in nodeData.childrenIndices)
+					{
+						nodeDict[child].parent = node;
+						node.children.Add(nodeDict[child]);
+					}
+				}
+
+				if (nodeData.parentIndex == -1)
+				{ // I am root!
+					root = (ICompositeNode)nodeDict[nodeData.index];
+					currentNode = root.Init();
+				}
+			}
+
 		}
 
 
